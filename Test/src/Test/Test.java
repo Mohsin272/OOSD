180a519d08e87ea26e9b07fe7fe3a8a6eb622d7d
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-07 11:41:12 +0100

    1 print

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -1,24 +1,11 @@
 package Test;
 
-<<<<<<< HEAD
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
-		System.out.println("1This is a test");
-		System.out.println("3This is a test");
-		Sysem.out.println("2nd print");
+		
 	}
 
-=======
-public class Test {
-  public static void main(String[] args){
-    System.out.println("This is a test");
-    Sysem.out.println("This is a test");
-    Sysem.out.println("Mohsin");
-    
-  }
-  
-  
->>>>>>> branch 'master' of https://github.com/Mohsin272/OOSD.git
+
 
 }
