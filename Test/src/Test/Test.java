97fd93e17aa180ba236548c43f0c7d46b9318eac
Mohsin272 <72495360+Mohsin272@users.noreply.github.com>
2020-10-07 11:13:37 +0100

    added change

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -4,7 +4,7 @@ public class Test {
   public static void main(String[] args){
     System.out.println("This is a test");
     Sysem.out.println("2nd print");
-    
+    Sysem.out.println("3rd print");
   }
   
   
