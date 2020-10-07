8e1b772168437154f65fb18c61b86a6b3093e9e8
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-08 00:05:27 +0100

    jlab1q1

--- jlab1/src/jlab1/thermtest.java
+++ jlab1/src/jlab1/thermtest.java
@@ -7,7 +7,7 @@ public class thermtest {
 
 			Thermometer thermA = new Thermometer();	// Create an instance of our Thermometer class
 			Thermometer thermB = new Thermometer(10);
-			Thermometer tempB = thermB;
+			//Thermometer tempB = thermB;
 			System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 			thermA.setCelsius(20.0);
 			System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
