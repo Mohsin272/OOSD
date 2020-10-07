5d466d0bfaab814271a08162a824d74d3651fcc0
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-07 11:04:10 +0100

    3rd line

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -1,5 +1,10 @@
 package Test;
 
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+		System.out.println("1This is a test");
+		System.out.println("3This is a test");
+	}
 
 }
