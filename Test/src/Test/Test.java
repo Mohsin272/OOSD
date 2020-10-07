2d6554e01cef3d102f2463249294a3c0ef1435ed
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-07 23:56:16 +0100

    jlab1q1

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -3,8 +3,6 @@ package Test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
-		System.out.println("2nd print");
-		System.out.println("3rd print");
 		
 	}
 
