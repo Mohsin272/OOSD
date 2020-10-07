dee7a78edb7fc72a7875548bbf41f0cafd5a123c
Mohsin272 <72495360+Mohsin272@users.noreply.github.com>
2020-10-07 11:21:54 +0100

    Update Test.java

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -3,8 +3,9 @@ package Test;
 public class Test {
   public static void main(String[] args){
     System.out.println("This is a test");
-    Sysem.out.println("2nd print");
-    Sysem.out.println("3rd print");
+    Sysem.out.println("This is a test");
+    Sysem.out.println("This is a test");
+    
   }
   
   
