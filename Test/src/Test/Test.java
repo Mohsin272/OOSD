50fbb6701fe2916245bfd5a80a9e9c6e52005285
Mohsin272 <72495360+Mohsin272@users.noreply.github.com>
2020-10-07 11:45:32 +0100

    Update Test.java

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
 		System.out.println("2nd print");
+		System.out.println("3rd print");
 		
 	}
 
