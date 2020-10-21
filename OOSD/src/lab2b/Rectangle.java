4e6342869af5769c9319c3a888ebc68698900101
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-21 10:51:32 +0100

    2b

--- OOSD/src/lab2b/Rectangle.java
+++ OOSD/src/lab2b/Rectangle.java
@@ -1,31 +1,21 @@
 package lab2b;
 
 public class Rectangle {
+	
 	//Variables
 	private double length;
 	private double width;
-	private double area;
-	private double perimeter;
-	//Contructors
+
+	
+	//Constructors
 	public Rectangle() {
-		setLength(6.0);
-		setWidth(4.0);
-		setArea(0);
-		setPerimeter(0);
+		setLength(1.0);
+		setWidth(1.0);
 	}
 	
 	public Rectangle(int length, int width) {
 		setLength (length);
 		setWidth(width);
-	}
-	
-	
-	public Rectangle(double length, double width, double area, double perimeter) {
-		super();
-		setLength (length);
-		setWidth (width);
-		setArea (area);
-		setPerimeter (perimeter);
 	}
 
 	//Getters && Setters
@@ -55,35 +45,20 @@ public class Rectangle {
 			System.out.println("width not in range");
 		}
 	}
-	public double getArea() {
-		return area;
-	}
 
-	public void setArea(double area) {
-		area=length*width;
-		this.area = area;
-	}
 
-	public double getPerimeter() {
-		return perimeter;
-	}
-
-	public void setPerimeter(double perimeter) {
-		perimeter=2*(length+width);
-		this.perimeter = perimeter;
-	}
 
 	//toString
 	public String toString() {
-		return "Rectangle [length=" + length + ", width=" + width + ", area=" + area + ", permimeter=" + perimeter
+		return "Rectangle [length=" + length + ", width=" + width 
 				+ "]";
 	}
 	
 	public void printRectangle(){
 		
-		for(int i=1;i<=length;i++) {//checks if length is index position in less than or = to length
+		for(int i=1;i<=length;i++) {//checks if length index position in less than or = to length
 			
-			for(int j=1;j<=width;j++) {
+			for(int j=1;j<=width;j++) {//checks if width index position in less than or = to width
 				
 				if(i==1||i==length||j == 1 || j == width) {
 					
@@ -96,6 +71,32 @@ public class Rectangle {
 			}
 			
 		}
+	
+	/*public void printRectangle() {
+		String w ="*";
+		for(int i=0;i<width-1;i++) {
+			w+="*";
+		}
+		System.out.println(w);
+		
+		for(int j=0;j<length-2;j++) {
+			System.out.println("*");
+			for(int x=0;x<width-2;x++) {
+				System.out.print(" ");
+			}
+			System.out.println("*");
+		}
+		System.out.println(w);
+			
+	}*/
+	
+	public double getArea() {
+		return length*width;
+	}
+	public double getPerimeter() {
+		return (length+width) *2;
+		
+	}
 		
 	}
 	
--- OOSD/src/lab2b/RectangleDriver.java
+++ OOSD/src/lab2b/RectangleDriver.java
@@ -5,6 +5,10 @@ public class RectangleDriver {
 	public static void main(String[] args) {
 		Rectangle rec1=new Rectangle();
 
+		rec1.setLength(5.0);
+		rec1.setWidth(10.0);
+		System.out.println("Area:"+ rec1.getArea());
+		System.out.println("Perimeter:"+ rec1.getPerimeter());
 		System.out.println(rec1.toString());
 		rec1.printRectangle();
 
