c939ab1fe4f2d4dd5b0d4985480c5d712db97443
Mohsin Tahir <Mohsin Tahir@192.168.1.21>
2020-10-21 11:17:48 +0100

    tickets

---
+++ OOSD/src/Programs/SoccerTickets.java
@@ -0,0 +1,18 @@
+package Programs;
+
+public class SoccerTickets {
+
+	public static void main(String[] args) {
+		
+		Ticket soccerTicket1=new Ticket();
+		Ticket soccerTicket2=new Ticket();
+		
+		System.out.println("Tickets Sold: "+ Ticket.getNoTicketsSold());
+		
+		System.out.println(soccerTicket1.getInfo());
+		System.out.println(soccerTicket2.getInfo());
+		
+		
+	}
+
+}
